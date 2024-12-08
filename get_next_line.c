005442d31a5e3ecf0d57a4cbf76a608692001bac
Kouta Sakatsume <kosakats@c2r7s9.42tokyo.jp>
2024-12-08 15:49:22 +0900

    1st_get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 11:24:41 by kosakats          #+#    #+#             */
-/*   Updated: 2024/11/20 21:40:17 by kosakats         ###   ########.fr       */
+/*   Updated: 2024/12/07 10:50:08 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,132 @@
 
 char	*get_next_line(int fd)
 {
-}
+	char		c;
+	t_string	ret;
 
-int	main(void)
-{
-	char	*line;
-	int		i;
-
-	int fd, fd2, fd3;
-	fd = open("text.txt", O_RDONLY);
-	fd2 = open("text2.txt", O_RDONLY);
-	fd3 = open("text3.txt", O_RDONLY);
+	ret.str = NULL;
+	ret.len = 0;
+	ret.capa = 0;
 	while (1)
 	{
-		printf("%s", get_next_line(fd));
-		printf("%s", get_next_line(fd2));
-		printf("%s", get_next_line(fd3));
-		if (!line)
-		{
-			free(line);
+		c = ft_getc(fd);
+		if (c == EOF)
 			break ;
-		}
-		free(line);
+		ft_putc(&ret, c);
+		if (c == '\n')
+			break ;
 	}
-	close(fd);
-	return (0);
+	if (ret.len > 0)
+		ft_putc(&ret, '\0');
+	if (ret.str == NULL || ret.len == 0)
+	{
+		free(ret.str);
+		return (NULL);
+	}
+	return (ret.str);
 }
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("text.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		perror("Failed to open file");
+// 		return (1);
+// 	}
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
+
+// int	main(void)
+// {
+// 	int	fd;
+// 	int	c;
+
+// 	fd = open("text.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		perror("Error opening file");
+// 		return (1);
+// 	}
+// 	while (1)
+// 	{
+// 		c = ft_getc(fd);
+// 		if (c == EOF)
+// 			break ;
+// 		printf("%c", c);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
+
+// #include "get_next_line.h"
+// #include <fcntl.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <unistd.h>
+
+// char	*get_next_line(int fd)
+// {
+// 	static int	n = 0;
+// 	static int	start = 0;
+// 	char		*line;
+// 	int			end;
+// 	static char	buf[BUFFER_SIZE];
+
+// 	if (n == 0)
+// 	{
+// 		n = read(fd, buf, BUFFER_SIZE);
+// 		if (n <= 0)
+// 			return (NULL);
+// 		start = 0;
+// 	}
+// 	end = start;
+// 	while (end < n && buf[end] != '\n')
+// 		end++;
+// 	line = malloc(end - start + 2);
+// 	if (!line)
+// 		return (NULL);
+// 	memcpy(line, buf + start, end - start);
+// 	line[end - start] = '\0';
+// 	if (end < n && buf[end] == '\n')
+// 	{
+// 		line[end - start] = '\n';
+// 		line[end - start + 1] = '\0';
+// 		start = end + 1;
+// 	}
+// 	else
+// 	{
+// 		n = 0;
+// 	}
+// 	return (line);
+// }
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = open("text.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		perror("Error opening file");
+// 		return (1);
+// 	}
+// 	while ((line = get_next_line(fd)))
+// 	{
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kosakats <kosakats@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 11:58:02 by kosakats          #+#    #+#             */
-/*   Updated: 2024/11/20 12:01:08 by kosakats         ###   ########.fr       */
+/*   Updated: 2024/12/07 10:46:05 by kosakats         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,26 @@
 # define GET_NEXT_LINE_H
 
 # include <fcntl.h>
+# include <stddef.h>
 # include <stdio.h>
+# include <stdlib.h>
 # include <string.h>
+# include <unistd.h>
+
+# ifdef BUFFER_SIZE
+#  undef BUFFER_SIZE
+# endif
+# define BUFFER_SIZE 1000000
+
+typedef struct s_string
+{
+	char	*str;
+	size_t	len;
+	size_t	capa;
+}			t_string;
+
+char		*get_next_line(int fd);
+int			ft_putc(t_string *str, char c);
+char		ft_getc(int fd);
 
 #endif
